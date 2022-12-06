8535efe9450f4142710048caa3a9b79d643511d1
Casper Kizewski <163021@student.horizoncollege.nl>
2022-12-06 14:59:00 +0100

    Creating the dashboard

--- MKB/resources/views/livewire/create.blade.php
+++ MKB/resources/views/livewire/create.blade.php
@@ -1,11 +1,15 @@
-<form>
-    <label for="title">Title</label>
-    <input type="text" name="title" id="postTitle">
-    <label for="image">Image</label>
-    <input type="text" name="image" id="postImage">
-    <label for="price">Price</label>
-    <input type="text" name="price" id="postPrice">
-    <label for="description">Description</label>
-    <textarea name="description" id="postDescription" cols="30" rows="10"></textarea>
-    <button type="submit">Submit</button>
+<form class="px-4 my-28 container mx-auto space-y-6">
+    <h1 class="text-2xl font-bold text-center">Create a new event</h1>
+    <hr/>
+    <div class="flex flex-row space-x-8 justify-center">
+        <input type="text" name="title" id="postTitle" placeholder="Title" class="border border-gray-300 p-2 rounded-md" wire:model="name">
+        <input type="text" name="image" id="postImage" placeholder="Image-URL" class="border border-gray-300 p-2 rounded-md" wire:model="image">
+        <input type="number" name="price" id="postPrice" placeholder="Price" class="border border-gray-300 p-2 rounded-md" wire:model="price">
+    </div>
+    <div class="flex flex-row space-x-8 justify-center">
+        <textarea name="description" id="postDescription" cols="50" rows="" wire:model="description" placeholder="Description" class="border border-gray-300 p-2 rounded-md"></textarea>
+    </div>
+    <div class="flex flex-row space-x-8 justify-center">
+        <button wire:click.prevent="store()" class="bg-[#343434] text-white py-2 px-5 rounded-md">Create</button>
+    </div>
 </form>
