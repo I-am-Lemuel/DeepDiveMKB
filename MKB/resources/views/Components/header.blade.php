5fca2c0f1b1fc1564189825e6b168b838f9f3fc2
David Ngo <164350@student.horizoncollege.nl>
2022-12-07 10:34:09 +0100

    created hero

--- MKB/resources/views/Components/header.blade.php
+++ MKB/resources/views/Components/header.blade.php
@@ -1,1 +1,14 @@
-<h2 class="">Header</h2>
+<h2 class="bg">IT</h2>
+
+<div class="bg-gray-100 dark:bg-transparent">
+    <div class="container mx-auto flex flex-col items-center py-12 sm:py-24">
+        <div class="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
+            <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 dark:text-white font-black leading-7 md:leading-10">
+                The Freedom to Create the
+                <span class="text-indigo-700">Websites</span>
+                You Want
+            </h1>
+            <p class="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">A professonal website drives sales. Create a beautiful website to impress and engage new customers and establish your business online </p>
+        </div>
+    </div>
+</div>
