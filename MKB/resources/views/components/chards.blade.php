4ef867331e2690fcecda856f85e3dee1756f50af
SudoLemuel <lembak03@hotmail.com>
2022-12-07 11:27:15 +0100

    new-card-desing

--- MKB/resources/views/components/chards.blade.php
+++ MKB/resources/views/components/chards.blade.php
@@ -1,6 +1,11 @@
-<div class="">
-    @for ($i = 0; $i < 20; $i++)
-        <div class="">
-        </div>
-    @endfor
+<div class="h-screen bg-[#123D7B] flex justify-center items-center flex-col">
+    <h2 class="p-11">
+        <span class="text-[#ffffff] text-4xl font-bold">Bounty's</span>
+    </h2>
+    <div class="flex flex-wrap justify-evenly w-4/5 h-4/5 bg-[#091833] border-2 border-[#EA00D9] overflow-scroll p-4">
+        @for ($i = 0; $i < 18; $i++)
+            <div class="flex w-3/12 m-8 h-3/4  bg-[#133E7C]">
+            </div>
+        @endfor
+    </div>
 </div>
