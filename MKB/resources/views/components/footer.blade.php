8c83338f1a5dbdd6b88a77ce4271ec1b2671b1c6
David Ngo <164350@student.horizoncollege.nl>
2022-12-07 11:29:45 +0100

    header update

--- MKB/resources/views/components/footer.blade.php
+++ MKB/resources/views/components/footer.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+ 
+        <div class="pt-12">
+            <footer id="footer" class="relative z-50 dark:bg-gray-900">
+                <div class="py-16 flex flex-col justify-center items-center">
+                    
+                </div>
+            </footer>
+        </div>
--- MKB/resources/views/website/website.blade.php
+++ MKB/resources/views/website/website.blade.php
@@ -11,5 +11,6 @@
 <h1>website.blade.php</h1>
 <x-header />
 <x-chards />
+<x-footer />
 </body>
 </html>
