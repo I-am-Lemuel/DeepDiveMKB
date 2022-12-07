81710fe4eb15a52922643eb7f86b15a78908e0d2
Lucien <lucien.v.d@outlook.com>
2022-12-07 10:07:53 +0100

    Header + website

--- MKB/resources/views/Components/Header.blade.php
+++ MKB/resources/views/Components/Header.blade.php
@@ -1,1 +1,5 @@
-<h2 class="bg-">Header</h2>
+<div class="x-h-max w-full bg-slate-500 flex justify-center">
+    <h1
+        class="place-self-center font-extrabold text-9xl text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+        ITPrize</h1>
+</div>
--- MKB/resources/views/website/website.blade.php
+++ MKB/resources/views/website/website.blade.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     @vite('resources/css/app.css')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <style>
+        body {
+            width: calc(100vw - (100vw - 100%));
+        }
+
+        .x-h-max {
+            height: 100vh;
+        }
+    </style>
 </head>
+
 <body>
-
-<h1>website.blade.php</h1>
-<x-header/>
-<x-chards/>
-
+    <x-header />
+    <x-chards />
 </body>
+
 </html>
