533041099718a8ce838527476177ec69b4293dc6
SudoLemuel <lembak03@hotmail.com>
2022-12-08 09:18:31 +0100

    scroll-snap

--- MKB/resources/views/components/footer.blade.php
+++ MKB/resources/views/components/footer.blade.php
@@ -1,8 +1,7 @@
- 
-        <div class="pt-12">
-            <footer id="footer" class="relative z-50 dark:bg-gray-900">
-                <div class="py-16 flex flex-col justify-center items-center">
-                    
-                </div>
-            </footer>
-        </div>
+ <div id="snap" class="pt-12">
+     <footer id="footer" class="relative z-20 dark:bg-gray-900">
+         <div class="py-16 flex flex-col justify-center items-center">
+
+         </div>
+     </footer>
+ </div>
--- MKB/resources/views/website/website.blade.php
+++ MKB/resources/views/website/website.blade.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     @vite('resources/css/app.css')
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link src="resources/css/card.css" rel="stylesheet">
     <title>IT-Price | Home</title>
 </head>
 
 <body>
-<x-header />
-<x-chards />
-<x-footer />
+    <div class="main-container">
+        <x-header />
+        <x-chards />
+        <x-footer />
+    </div>
 </body>
 
 </html>
