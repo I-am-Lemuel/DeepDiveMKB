71efa396a2b7a48e5b27d31ee5ca2a2e60c7c941
Dylan Kuiper <165335@student.horizoncollege.nl>
2022-12-08 09:37:29 +0100

    footer links

--- MKB/resources/views/components/footer.blade.php
+++ MKB/resources/views/components/footer.blade.php
@@ -1,8 +1,13 @@
- 
-        <div class="pt-12">
-            <footer id="footer" class="relative z-50 dark:bg-gray-900">
-                <div class="py-16 flex flex-col justify-center items-center">
-                    
-                </div>
-            </footer>
+<div class="bg-linear-pink-invert py-3 px-1">
+    <div tabindex="0" aria-label="footer" class="focus:outline-none mx-auto container flex flex-col items-center justify-center">
+        <div class="text-black flex flex-col md:items-center f-f-l pt-3">
+            <div class="my-6 text-base text-color f-f-l"> 
+                <ul class="md:flex items-center">
+                    <li class="md:mr-6 cursor-pointer text-2xl pt-4 lg:py-0"><a href="https://www.notion.so/30b1b2f93e4f490f81d6f290128b0e50?v=43c0792c119d412c91966f87c1ae0b8e" class="focus:outline-none dark:text-white  focus:underline hover:text-gray-500">Notion </a></li>
+                    <li class="md:mr-6 cursor-pointer text-2xl pt-4 lg:py-0"><a href="https://github.com/I-am-Lemuel/DeepDiveMKB" class="focus:outline-none dark:text-white  focus:underline hover:text-gray-500">github </a></li>
+                    <li class="md:mr-6 cursor-pointer text-2xl pt-4 lg:py-0"><a href="link naar dashboard" class="focus:outline-none dark:text-white  focus:underline hover:text-gray-500">dashboard </a></li>
+                </ul>
+            </div>
         </div>
+    </div>
+</div>  
