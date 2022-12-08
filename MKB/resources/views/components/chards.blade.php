c2280bc9b1005ab027b3d433a0d570800b849ebd
Casper Kizewski <163021@student.horizoncollege.nl>
2022-12-08 10:42:29 +0100

    cards snap

--- MKB/resources/views/components/chards.blade.php
+++ MKB/resources/views/components/chards.blade.php
@@ -1,12 +1,21 @@
-<div id="snap" class="h-screen bg-[#091833] flex justify-center items-center flex-col">
-    <h2 class="p-11">
-        <span class="text-[#ffffff] text-4xl font-bold">Bounty's</span>
-    </h2>
-    <div class="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 bg-[#091833] overflow-auto border-b-2 border-[#EA00D9] p-4 container">
-        @for ($i = 0; $i < 5; $i++)
-            <div class="h-96 bg-black">
-                sheesh
+<div class="flex justify-evenly flex-wrap bg-gradient-to-r from-[#383677] to-[#091833] min-h-full w-full">
+    @for ($i = 0; $i < 20; $i++)
+        <div class="flex justify-center m-2">
+            <div class="rounded-lg shadow-lg bg-[#0abdc6] max-w-sm">
+                <a href="#!">
+                    <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                        alt="" />
+                </a>
+                <div class="p-6">
+                    <h5 class="text-[#ffffff] text-xl font-medium mb-2">Card title</h5>
+                    <p class="text-[#ffffff] text-base mb-4">
+                        Some quick example text to build on the card title and make up the bulk of the card's
+                        content.
+                    </p>
+                    <button type="button"
+                        class=" inline-block px-6 py-2.5 bg-[#EA00D9] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
             </div>
-        @endfor
-    </div>
+        </div>
+    @endfor
 </div>
