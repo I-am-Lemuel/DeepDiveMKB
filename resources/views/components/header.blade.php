ce0312da163b4e5248466205d6de06175ee01e54
Casper Kizewski <163021@student.horizoncollege.nl>
2022-12-09 13:33:06 +0100

    Title Change

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,12 +2,10 @@
     <div class="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
         <h1
             class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-white font-black leading-7 md:leading-10">
-            The Freedom to Create the
-            <span class="text-[#EA00D9]">Websites</span>
-            You Want
+            Welkom bij
+            <span class="text-[#EA00D9]">IT-Prize</span>
         </h1>
-        <p class="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">A professonal
-            website drives sales. Create a beautiful website to impress and engage new customers and establish your
-            business online </p>
+        <p class="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">Een omgeving
+        om jou oplossingen te vinden. </p>
     </div>
 </div>
